db7bd2fd20dad5029b196870a5af41273e127a2a
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-12-07 21:28:12 -0500

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,5 +1,16 @@
 
-def fibnum(n):
+
+def main():
+    number = int(input("Please Enter integer number: "))
+    print("The 9th element of the Fibonnaci sequence is : {}".format(fibonacci(number)))
+    s1 = input("Please Enter the first string: ")
+    s2 = input("Please Enter the second string: ")
+    print(compareTo(s1,s2))
+    a=int(input("Please Enter the first integer: "))
+    b=int(input("Please Enter the second integer: "))
+    print("The GCD is {} ".format(gcd(a,b)))
+
+def fibonnaci(n):
 
     if n == 0:
         return n
@@ -8,12 +19,30 @@ def fibnum(n):
         return 1
 
     else:
-        return (fibnum(n - 1) + fibnum(n - 2))
+        return (fibonnaci(n - 1) + fibonnaci(n - 2))
 
-def GCD(a, b):
+def gcd(a, b):
 
     if b == 0:
         return a
     else:
-        return GCD(b, a % b)
+        return gcd(b, a % b)
 
+def compareTo(s1, s2):
+
+    if len(s1) == 0 and len(s2) == 0:
+        return 0
+    elif len(s1) == 0:
+        return -1
+    elif len(s2) == 0:
+        return 1
+
+    if s1[0] < s2[0]:
+        return -1
+    elif s1[0] > s2[0]:
+        return 1
+    else:
+        return compareTo(s1[1:], s2[1:])
+
+if __name__ == '__main__':
+    main()
